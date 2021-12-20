10db8bd23ddcf5210bdf88f4febaaf3757eea85a
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2021-12-20 12:24:30 +0530

    Fix drawing area
    Fixed the size and position of the grid
    area.
    Basic drawing function is implemented.
    Need to add the erasing feature.
    Need to add change colour feature.
    Need to make the page more attractive.

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ container.style.width = '400px';
 container.style.height = '500px';
 
 function setUpGrid() {
-    for(let i=0; i<16*16; i++) {
-        const gridElement = document.createElement('div');
-        gridElement.className = "gridElement";
-        gridElement.addEventListener('mouseover', ()=>{
-            gridElement.style.background = "white";
-        });
-        container.appendChild(gridElement);
+    for(let i=0; i<25; i++) {
+        for(let j=0; j<25; j++) {
+            const gridElement = document.createElement('div');
+            gridElement.className = "gridElement";
+            gridElement.addEventListener('mouseover', () => {
+                gridElement.style.background = 'black';
+            });
+            container.appendChild(gridElement);
+        }
     }
 }
-
 
 window.onload(setUpGrid());
